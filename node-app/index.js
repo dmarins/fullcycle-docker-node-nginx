314912bed397fc3f00d0dd0a21d9cb815b16b909
Diogo Marins <diogodsfmarins@gmail.com>
2023-06-11 15:02:21 -0300

    refactor: add dockerize and changing index.js

--- node-app/index.js
+++ node-app/index.js
@@ -2,35 +2,40 @@ const express = require('express');
 const app = express();
 
 const config = {
-    host: 'db',
-    port: '3306',
-    user: 'root',
-    password: 'root',
-    database: 'nodedb'
-  }
+  host: 'db',
+  port: '3306',
+  user: 'root',
+  password: 'root',
+  database: 'nodedb'
+}
 const mysql = require('mysql');
-const conn = mysql.createConnection(config);
 
 app.get('/', async (_, res) => {
-  const selectSql = `SELECT * FROM people`;
-  const people = conn.query(selectSql);
-
-  const title = '<h1>Full Cycle Rocks!</h1>';
-  const list = `
-    <ul>
-      ${people.map(p => `<li>${p.name}</li>`).join('')}
-    </ul>
-  `;
-
-  res.send(title + list);
+  const conn = mysql.createConnection(config);
+  conn.query(
+    'Select * from people;',
+    function (error, results, fields) {
+      let text = '<h1>Full Cycle Rocks!</h1><br /><ul>';
+      for (const user of results) {
+        text += `<li>${user.name}</li>`;
+      }
+      text += '</ul>';
+    
+      return res.send(text);
+    }
+  );
 });
 
 app.listen(3000, () => {
   console.log('Running on port 3000');
 
+  const conn = mysql.createConnection(config);
+
   const createSql = `CREATE TABLE IF NOT EXISTS people (id INT NOT NULL AUTO_INCREMENT, name VARCHAR(50), PRIMARY KEY (id));`;
   conn.query(createSql);
-  
+
   const insertSql = `INSERT INTO people (name) values ('John Doe');`;
   conn.query(insertSql);
+
+  conn.end();
 });
